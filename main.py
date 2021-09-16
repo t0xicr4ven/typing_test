5d1a17b1c287d57466e307f0304b38d97eeb4cf3
t0xicr4ven <57691355+t0xicr4ven@users.noreply.github.com>
2021-09-16 15:55:17 +1000

    added entry

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from tkinter import *
 import random
 
 rand_num_for_para = random.randint(1,5)
-
+FONT=('Courier',18)
 
 def set_starting_wrds():
     test_text = []
@@ -11,11 +11,8 @@ def set_starting_wrds():
         for line in file:
             word = line.splitlines()
             test_text.append(word)
-
-    print(test_text)
     for i in range(0,17):
         new_wrd = random.choice(test_text)[0]
-        print(new_wrd)
         test_str = test_str + new_wrd + ' '
     return test_str
 
@@ -24,12 +21,10 @@ starting_string = set_starting_wrds()
 window = Tk()
 window.title('Typing Speed Test')
 t = Text(wrap=WORD)
-#canvas = Canvas(width=600, height=600)
-#canvas.grid(column=0, row=0)
-t.configure(font=('Courier',18))
+t.configure(font=FONT)
 t.insert(INSERT, starting_string)
 t.grid(column=0, row=0, padx=10, pady=10)
-#test_text = canvas.create_text(300,200, text=t)
-#test_text.grid(column=0, row=0)
+entry = Entry(window, width=100, font=FONT)
+entry.grid(column=0,row=1, rowspan=2, padx=10, pady=10)
 
 window.mainloop()
